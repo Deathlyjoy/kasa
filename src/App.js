b15354ae7f0593ac6a0882b17a554e9c6bf5a171
Deathlyjoy <colombelle.quentin@icloud.com>
2023-08-16 07:37:34 +0200

    Initial React Router App.js
    Ajout des composant BrowserRouter et Route dans le fichier App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './assets/logo.svg';
+// ----- Import de React, BrowserRouter, Routes et Route depuis la bibliothèque react-router-dom -----
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// ----- Import fichier de style -----
 import './style/App.css';
+
+// Import des composants de pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Location from "./pages/Location";
+import Error from "./pages/Error";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter> {/* Utilisation du composant BrowserRouter pour gérer la navigation */}
+      <Routes> {/* Utilisation du composant Routes pour définir les routes */}
+        <Route path="/" element={<Home />} /> {/* Route pour la page d'accueil */}
+        <Route path="/about" element={<About />} /> {/* Route pour la page "A Propos" */}
+        <Route path="/location/:id" element={<Location />} /> {/* Route pour une page d'hébergement spécifique */}
+        <Route path="*" element={<Error />} /> {/* Route pour toutes les autres URL (erreur 404) */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
